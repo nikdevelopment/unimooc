67ef4d8beabdf430827c7a20bc9d08ecfa3d9c21
Nik Muhammad Firdaus <nmfirdaus@outlook.com>
2021-04-28 11:51:46 +0800

    app.css

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front_end.login');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
