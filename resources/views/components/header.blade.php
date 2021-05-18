53be6b2fb2b23f712bbc348b1ead736607649ce6
linode <nikdevelopment.com>
2021-05-18 02:39:40 +0000

    Latest

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,12 @@
+<div class="flex w-full h-14 bg-white justify-between">
+    <div class="flex w-96 h-full">
+        <div class="flex w-14 h-14 bg-teal justify-center items-center cursor-pointer" id="nav_button">
+            <div class="flex w-7 h-7"><x-svg.hamburger /></div>
+        </div>
+        <div class="flex w-40 h-full bg-gray-300">
+        
+        </div>
+    </div>
+    <div class="flex w-96 h-full bg-green-400"></div>
+    <div class="flex w-96 h-full bg-green-400"></div>
+</div>
